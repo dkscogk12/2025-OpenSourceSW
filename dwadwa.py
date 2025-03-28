d58a507d8b68e37d4d73bc4c918bf56f24166474
dkscogk12 <jeondaeho205@gmail.com>
2025-03-28 11:01:09 +0900

    수정 완료

--- dwadwa.py
+++ dwadwa.py
@@ -1,4 +1,5 @@
-x = int(input('숫자 1 입력 >> '))
-y = int(input('숫자 2 입력 >> '))
+a = int(input('숫자 1 입력 >> '))
+b = int(input('숫자 2 입력 >> '))
 
-print(f'{x}+{y} = {x+y}')
+print(f'{a}x{b} = {a*b}')
+#수정완료
